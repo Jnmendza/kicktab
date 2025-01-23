2b926966d8e3229df8bc57543cf8b709dbc704e4
Jonathan Mendoza <jonatmendoza@gmail.com>
2025-01-22 18:37:50 -0800

    built out useAuth hook

--- src/hooks/useAuthStore.ts
+++ src/hooks/useAuthStore.ts
@@ -1,1 +1,19 @@
-// hook for zustand
+import { create } from "zustand";
+
+interface AuthState {
+  user: {
+    id: string;
+    email: string;
+    name: string;
+  } | null;
+  isAuthenticated: boolean;
+  setUser: (user: { id: string; email: string; name: string }) => void;
+  lougout: () => void;
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+  isAuthenticated: false,
+  setUser: (user) => set({ user, isAuthenticated: true }),
+  lougout: () => set({ user: null, isAuthenticated: false }),
+}));
