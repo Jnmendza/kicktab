e449ba3165fd8d247f0ea12dd447ed2e819355ad
Jonathan Mendoza <jonatmendoza@gmail.com>
2025-02-04 16:26:17 -0800

    search tests passed

--- src/components/Search/SearchContainer.tsx
+++ src/components/Search/SearchContainer.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import React from "react";
 import { useState } from "react";
 import Search from "./Search";
 import { Card } from "../ui/card";
--- src/jest.setup.ts
+++ src/jest.setup.ts
@@ -1,1 +1,13 @@
 import "@testing-library/jest-dom";
+
+if (typeof ResizeObserver === "undefined") {
+  global.ResizeObserver = class {
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+  };
+}
+
+if (!Element.prototype.scrollIntoView) {
+  Element.prototype.scrollIntoView = jest.fn();
+}
